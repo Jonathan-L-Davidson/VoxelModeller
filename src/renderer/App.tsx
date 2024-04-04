340297b47448692e5b1339bee19a71d18e7922a8
Jonathan-L-Davidson <--replace-all>
2024-04-05 00:25:49 +0100

    Final part of the tutorial!

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,16 +1,29 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
+import Alert from '../components/Alert';
 import ListGroup from '../components/ListGroup';
 import 'bootstrap/dist/css/bootstrap.css';
 
 function Hello() {
   const items = ['New York', 'San Francisco', 'Tokyo', 'London', 'Paris'];
+  const [selectedCity, setSelectedCity] = useState('Warning');
+
+  const [alertVisible, setAlertVisability] = useState(true);
+  const HandleAlertButton = () => setAlertVisability(false);
 
   const handleSelectItem = (item: string) => {
-    console.log(item);
+    setSelectedCity(item);
+    setAlertVisability(true);
   };
 
   return (
     <div>
+      {alertVisible && (
+        <Alert onClickedOn={HandleAlertButton}>
+          <strong>{selectedCity}!</strong>
+        </Alert>
+      )}
+
       <ListGroup items={items} title="Cities" onSelectItem={handleSelectItem} />
     </div>
   );
